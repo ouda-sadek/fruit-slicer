a8ba6840837cfb03f3ccdc73627f4dd6ff030a23
ouda-sadek <ouda.sadek@laplateforme.io>
2025-02-02 03:15:38 +0100

    replace fruit_type with object_type

--- game/entities/fruit.py
+++ game/entities/fruit.py
@@ -6,44 +6,45 @@ import os
 import random
 import string
 from os import *
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 from config import *
 
 class Fruit:
-    def __init__(self, x, y, size, speed, fruit_type):
+    def __init__(self, x, y, size, speed, object_type):
         self.x = x
         self.y = y
         self.size = size
         self.speed = speed
         self.letter = random.choice(string.ascii_uppercase)
-        self.fruit_type = fruit_type
+        self.object_type = object_type
         self.image = None                            
         self.rect = None
         self.is_sliced = False  
-        self.load_image(fruit_type)
+        self.load_image(object_type)
         if self.image is None:
-            print(f"Error: Could not load image for {fruit_type}. Object not created.")
+            print(f"Error: Could not load image for {object_type}. Object not created.")
             return
         self.rect = self.image.get_rect(center=(self.x, self.y))
 
-    def load_image(self, fruit_type):
-        fruit_image_files = [
-            f"{fruit_type}.png",  # Extension PNG
-            f"{fruit_type}.jpg",  # Extension JPG
-            f"{fruit_type}.jpeg", # Extension JPEG
-            f"{fruit_type}.bmp",  # Extension BMP
-            f"{fruit_type}.gif",  # Extension GIF
-            f"{fruit_type}.webp", # Extension WEBP
+    def load_image(self, object_type):
+        image_files = [
+            f"{object_type}.png",  # Extension PNG
+            f"{object_type}.jpg",  # Extension JPG
+            f"{object_type}.jpeg", # Extension JPEG
+            f"{object_type}.bmp",  # Extension BMP
+            f"{object_type}.gif",  # Extension GIF
+            f"{object_type}.webp", # Extension WEBP
         ]
         # Check if image exists in assets folder
         image_path = None
-        for filename in fruit_image_files:
+        for filename in image_files:
             image_path = os.path.join(IMAGE_FOLDER, filename)
             if os.path.exists(image_path):
                 break
         else:
             # If no images were found
-            print(f"Error: Image file for {fruit_type} not found in any expected formats.")
+            print(f"Error: Image file for {object_type} not found in any expected formats.")
             self.image = None
             return
         
@@ -53,9 +54,9 @@ class Fruit:
             image = pygame.image.load(image_path).convert_alpha()
             image = pygame.transform.scale(image, (self.size, self.size))
             self.image = image
-            print(f"Image loaded for {fruit_type} with size : {self.image.get_size()}")
+            print(f"Image loaded for {object_type} with size : {self.image.get_size()}")
         except Exception as e:
-            print(f"An error occurred while loading image for fruit type '{fruit_type}': {e}")
+            print(f"An error occurred while loading image for object type '{object_type}': {e}")
             self.image = None
         
     def update(self):
@@ -70,12 +71,4 @@ class Fruit:
             letter_text = self.font.render(self.letter, True, WHITE)  
             letter_rect = letter_text.get_rect(center=(self.rect.centerx, self.rect.centery - self.size // 2 - 10))  
             screen.blit(letter_text, letter_rect)
-
-            #pygame.display.update()
-        
-    def is_sliced(self, knife):
-        if knife.is_cutting:
-            return self.rect.colliderect(knife.rect)
-        else:
-            return False    
         
